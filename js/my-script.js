97c9f55e9b33418a2c6f324102d4f9804761a7b5
Peter Šutarík <sutarik.peter@gmail.com>
2017-03-21 14:51:00 +0100

    fix2

--- js/my-script.js
+++ js/my-script.js
@@ -1,3 +1,5 @@
-jQuery.get('http://sutarik.github.io/data/boilerplate.json', function(data) {
-    alert(data);
-});
+function getJSON(){
+    jQuery.get('http://sutarik.github.io/data/boilerplate.json', function(data) {
+        alert(data);
+    }); 
+}
