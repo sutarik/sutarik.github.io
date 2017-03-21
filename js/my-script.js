9e9e7c5fb2c72e1e5bcfde69b387f4505afe2ba8
Peter Šutarík <sutarik.peter@gmail.com>
2017-03-21 15:09:46 +0100

    sss

--- js/my-script.js
+++ js/my-script.js
@@ -1,7 +1,9 @@
+var tags, triggers, variables;
+
 $.getJSON("./data/boilerplate.json", function(json) {
     console.log(json); // this will show the info it in firebug console
 
-    var tags = json['tag'];
-    var triggers = json['trigger'];
-    var variables = json['variable'];
+    tags = json['tag'];
+    triggers = json['trigger'];
+    variables = json['variable'];
 });
