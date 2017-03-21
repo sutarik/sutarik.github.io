26cfc51c90054f0f76e22beba3d25492e01aa1c1
Peter Šutarík <sutarik.peter@gmail.com>
2017-03-21 15:14:25 +0100

    s

--- js/my-script.js
+++ js/my-script.js
@@ -2,7 +2,7 @@ var tags, triggers, variables;
 var obj;
 
 $.getJSON("./data/boilerplate.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
+    console.log(json['tag']); // this will show the info it in firebug console
     obj = json;
 
     tags = json['tag'];
