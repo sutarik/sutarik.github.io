2ad4d9796bfabccf0202a3b7a688a6e5c93c945f
Peter Šutarík <sutarik.peter@gmail.com>
2017-03-21 15:50:12 +0100

    edit

--- js/my-script.js
+++ js/my-script.js
@@ -1,7 +1,7 @@
 var obj;
 var tags, triggers, variables;
 
-var tagId = [], variableId = [], triggerId = [], folderId = [];
+var tagIds = [], variableIds = [], triggerIds = [], folderIds = [];
 
 $.getJSON("./data/boilerplate.json", function(json) {
     console.log(json.containerVersion.tag); // this will show the info it in firebug console
@@ -21,15 +21,29 @@ function getTagId(){
         var name = tags[i].name;
         var id = tags[i].tagId;
         obj[name] = id;
-        tagId.push(obj);
+        tagIds.push(obj);
     }
 }
 
 function getVariableId(){
+    for(var i=0;i<variables.length;i++){
+        var obj = {};
+        var name = variables[i].name;
+        var id = variables[i].variableId;
+        obj[name] = id;
+        variableIds.push(obj);
+    }
 
 }
 
 function getTriggerId(){
+    for(var i=0;i<triggers.length;i++){
+        var obj = {};
+        var name = triggers[i].name;
+        var id = triggers[i].triggerId;
+        obj[name] = id;
+        triggerIds.push(obj);
+    }
 
 }
 
@@ -40,4 +54,8 @@ function getFolderId(){
 function generateJSON(){
     // TODO
     // go through all the checked checkboxes
+    // check their arrays of tags ids
+    // store those ids to array
+    // go through these tags and create array of triggers which are usedd
+    // go through tags and triggers and indentify valu
 }
