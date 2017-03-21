844fcf156fc54510408b41418a4ce63f326b9592
Peter Šutarík <sutarik.peter@gmail.com>
2017-03-21 15:30:32 +0100

    Adding stuff

--- js/my-script.js
+++ js/my-script.js
@@ -1,5 +1,7 @@
-var tags, triggers, variables;
 var obj;
+var tags, triggers, variables;
+
+var tagId, variableId, triggerId, folderId;
 
 $.getJSON("./data/boilerplate.json", function(json) {
     console.log(json.containerVersion.tag); // this will show the info it in firebug console
@@ -8,4 +10,25 @@ $.getJSON("./data/boilerplate.json", function(json) {
     tags = json.containerVersion.tag;
     triggers = json.containerVersion.trigger;
     variables = json.containerVersion.variable;
+
+    getTagId();
 });
+
+function getTagId(){
+    // iterate through array with tags and store their Id's
+    for(var i=0;i<tags.length;i++){
+        tagId.push(tags[i].tagId);
+    }
+}
+
+function getVariableId(){
+
+}
+
+function getTriggerId(){
+
+}
+
+function getFolderId(){
+
+}
