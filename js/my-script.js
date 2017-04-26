8dcb3368ff4f33e0595029cf509c1b700d7b2708
Peter Šutarík <sutarik.peter@gmail.com>
2017-04-26 13:12:05 +0200

    as

--- js/my-script.js
+++ js/my-script.js
@@ -357,6 +357,22 @@ function modifyObject(){
     var newJSON = JSON.stringify(obj); // '{"name":"binchen"}'
     console.log(newJSON);
     $("#finalJSON").val(newJSON);
+
+    //create new file
+    var textFile = null;
+    var data = new Blob([newJSON], {type: 'text/plain'});
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+
+    textFile = window.URL.createObjectURL(data);
+
+    var link = document.getElementById('downloadlink');
+    link.href = textFile;
+    link.style.display = 'block';
 }
 
 
